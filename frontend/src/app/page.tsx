b989825bafb5d43f88fdfc1343e7693de82a4846
S1lva <diogomcsilva5@gmail.com>
2026-02-05 03:44:16 +0000

    feat: add fake login flow with localStorage and redirect

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,42 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const router = useRouter();
+  
+  useEffect(() => {
+    let savedEmail = localStorage.getItem("email");
+    if(savedEmail){
+      router.push("/dashboard");
+    }
+  }, [])
+
+
+  function addEmail(){
+      localStorage.setItem("email", email)
+      router.push("/dashboard");
+  }
+  
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
+        <form onSubmit={
+          e => {
+            e.preventDefault();
+            addEmail()
+          }
+        }>
+          <label htmlFor="email"></label>
+          <input type="email" id="email" placeholder="test@gmail.com" value={email} onChange={e => setEmail(e.target.value)}/>
+          <label htmlFor="pass"></label>
+          <input type="password" id="pass" placeholder="Senha123#" value={password} onChange={e => setPassword(e.target.value)}/>
+          <input type="submit" value="Submit" />
+        </form>
       </main>
     </div>
   );
