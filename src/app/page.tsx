0e6966246af5d4f6d08b7376b2391b31babf88e9
S1lva <diogomcsilva5@gmail.com>
2026-02-07 20:16:56 +0000

    feat: use Input and Button on LoginPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { setStorageItem, getStorageItem } from "../utils/storage"
+import Input from "../components/Input";
+import Button from "../components/Button";
+
+
 export default function Home() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -30,11 +34,11 @@ export default function Home() {
             addEmail()
           }
         }>
-          <label htmlFor="email"></label>
-          <input type="email" id="email" placeholder="test@gmail.com" value={email} onChange={e => setEmail(e.target.value)}/>
-          <label htmlFor="pass"></label>
-          <input type="password" id="pass" placeholder="Senha123#" value={password} onChange={e => setPassword(e.target.value)}/>
-          <input type="submit" value="Submit" />
+          <label htmlFor="email">Email</label>
+          <Input type="email" Icon="alternate_email" id="email" placeholder="test@gmail.com" value={email} onChange={e => setEmail(e.target.value)}/>
+          <label htmlFor="pass">Password</label>
+          <Input type="password" Icon="lock" id="pass" placeholder="Senha123#" value={password} onChange={e => setPassword(e.target.value)}/>
+          <Button type="submit" value="Log in" />
         </form>
       </main>
     </div>
